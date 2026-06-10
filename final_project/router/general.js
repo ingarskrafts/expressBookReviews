446ea19df480345f746220515e6f8da108d7feb1
ingarskrafts <ingarskrafts@gmail.com>
2026-06-10 06:08:47 -0400

    task1_bugfix

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,13 +6,12 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-    return res.send(JSON.stringify(books, null, 4));
+    //Write your code here
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
