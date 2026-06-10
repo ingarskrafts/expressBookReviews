eda977319107b0ebcc3afe79faea56be9cd195d8
ingarskrafts <ingarskrafts@gmail.com>
2026-06-10 06:26:17 -0400

    task2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,8 +16,12 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        return res.send(JSON.stringify(books[isbn], null, 4));
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
  });
   
 // Get book details based on author
