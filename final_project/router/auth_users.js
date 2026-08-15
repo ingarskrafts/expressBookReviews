4098c6252596026ee7ec6f73f27e5043687a395f
ingarskrafts <ingarskrafts@gmail.com>
2026-08-15 10:02:46 -0400

    task8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,10 +49,26 @@ regd_users.post("/login", (req,res) => {
   }
 });
 
-// Add a book review
+// Add or modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+
+    if (!books[isbn]) {
+        return res.status(400).json({message: "Book not found"});
+    }
+
+    if (!review) {
+        return res.status(400).json({message: "Review is required"});
+    }
+
+    books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: `Review for ISBN ${isbn} has been added/updated`,
+    reviews: books[isbn].reviews
+    });
 });
 
 module.exports.authenticated = regd_users;
