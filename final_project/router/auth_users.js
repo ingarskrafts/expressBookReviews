933c6840ec822cb8ebea589fb6b3a39527f6e6a1
ingarskrafts <ingarskrafts@gmail.com>
2026-08-20 03:47:53 -0400

    task9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -71,6 +71,28 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     });
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    if(!(books[isbn])) {
+        return res.status(404).json({message: "Book not found"});
+    }
+
+    if(!(books[isbn]).reviews[username]) {
+        return res.status(404).json({message: "Review not found for this user"});
+    }
+
+
+    delete books[isbn].reviews[username];
+
+    return res.status(200).json({
+        message: `Review for ISBN ${isbn} deleted by user ${username}`,
+        reviews: books[isbn].reviews
+    });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
